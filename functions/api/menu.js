302a083650042ae2508fdad7f9ed4f16a75d3250
Sky31Coffee <bocou2017@gmail.com>
2026-07-02 00:16:39 +0800

    Add files via upload

--- functions/api/menu.js
+++ functions/api/menu.js
@@ -21,7 +21,7 @@ export async function onRequest(context) {
     const visible = publicLimitedItemsV187(config);
     return json({
       ok: true,
-      version: "V214",
+      version: "V215",
       limitedItems: visible,
       currentLimitedBeanId: visible[0] ? visible[0].id : "",
       currentLimitedBeanIds: visible.map(item => item.id),
